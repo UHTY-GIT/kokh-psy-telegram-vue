3dccbd3396f6948e2468b4029504ab35c9e3a33e
Denys Balazh <73472742+denysitis@users.noreply.github.com>
2024-06-27 20:07:31 +0300

    add dist and node modules-1

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: '0.0.0.0', // Дозволяє доступ з будь-якої адреси
+    port: 80,
+    //host: '0.0.0.0', // Дозволяє доступ з будь-якої адреси
     //public: 'assistant.psy-kokh.online:8080', // Замініть 'your-remote-address:port' на вашу IP-адресу та порт, наприклад, '192.168.1.5:8080'.
     // Вимикає перевірку хоста, потрібно для доступу через будь-яку адресу
     // Увімкніть HTTPS, якщо це необхідно, наприклад, для тестування функціоналу, що вимагає безпечного з'єднання:
-    https: true,
-    allowedHosts: 'all', // Це дозволить доступ з будь-якого хоста
+    //https: true,
+    //allowedHosts: 'all', // Це дозволить доступ з будь-якого хоста
   }
 })
